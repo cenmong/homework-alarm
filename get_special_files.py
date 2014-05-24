738a518f4ac612edad6ae0eaf83acdd94e376839
cenmong <cenmong@gmail.com>
2014-05-24 22:17:46 +0800

    add 6 more events

--- get_special_files.py
+++ get_special_files.py
@@ -1,25 +1,34 @@
 import os
 from env import hdname
 
-clean = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1]
+'''
+0:ref; 1:slammer; 2:coast BO; 3:Katrina; 4:LA BO; 5:TW cabel; 6:Mid cable 1;
+7:Mid cable 2; 8:JP tsunami; 9:Spamhaus; 10:AU route leak; 11: Canada route
+leak; 12:SC earthquake; 13:Chile earthquake; 14:Sandy; 15:Northeast US BO;
+16:sea-me cable 17:TL BO
+'''
+clean = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0]
 
 ym = ['2008.12', '2003.01', '2003.08', '2005.09', '2005.08', '2006.12',\
-   '2008.01', '2008.12', '2011.03', '2013.03', '2012.02', '2012.08']
+    '2008.01', '2008.12', '2011.03', '2013.03', '2012.02', '2012.08',\
+   '2008.05', '2010.02', '2012.10', '2013.02', '2010.04', '2013.05']
 
 ymd1 = ['20081212', '20030124', '20030813', '20050910', '20050828', '20061225',\
-     '20080129', '20081218', '20110310', '20130310', '20120221', '20120807']
+     '20080129', '20081218', '20110310', '20130310', '20120221', '20120807',\
+     '20080510', '20100226', '20121020', '20130207', '20100412', '20130520']
 
 ymd2 = ['20081214', '20030126', '20030815', '20050912', '20050830', '20061227',\
-     '20080131', '20081220', '20110312', '20130330', '20120224', '20120810']
+     '20080131', '20081220', '20110312', '20130330', '20120224', '20120810',\
+     '20080515', '20100228', '20121031', '20130211', '20120417', '20130524']
 
 def get_file():
-    for i in range(3, len(ym)):  # Flexible: just get what I need
+    for i in range(len(ym)-1, len(ym)):  # Flexible: just get what I need
         os.system('lynx -dump http://archive.routeviews.org/bgpdata/' + ym[i] +\
                 '/UPDATES/ > tmpfile')
         f = open('tmpfile', 'r')
-        #if os.path.exists('metadata/files' + ymd1[i]):
-            #os.system('rm metadata/files' + ymd1[i])
-        #flist = open('metadata/files' + ymd1[i], 'a')
+        if os.path.exists('metadata/files' + ymd1[i]):
+            os.system('rm metadata/files' + ymd1[i])
+        flist = open('metadata/files' + ymd1[i], 'a')
         for line in f.readlines():
             if line.split('.')[-1] != 'bz2\n':
                 continue
@@ -35,7 +44,7 @@ def get_file():
             else:
                 print 'exists!'
                 continue
-            #flist.write(topofile.replace('.bz2', '.psd') + '\n')  # Names of parsed files are stored
+            flist.write(topofile.replace('.bz2', '.psd') + '\n')  # Names of parsed files are stored
             if os.path.exists(hdname + topofile):
                 try:
                     os.system('bunzip2 ' + hdname + topofile)  # Unpack the files
@@ -58,7 +67,7 @@ def get_file():
                 os.system('python ~/Downloads/clean.py ' + hdname + topofile +\
                         '.psd')
         f.close()
-        #flist.close()
+        flist.close()
         os.system('rm tmpfile')
     return
 
--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from get_special_files import ymd1
 
 #filelist = 'metadata/test-files20050828'
 #filelist = 'metadata/hk-localtest'
-for i in range(0, len(ymd1)):
-    if i == 9:
-        continue
+for i in range(len(ymd1)-6, len(ymd1)):
     filelist = 'metadata/files' + ymd1[i]
     ana = Analyzer(filelist, 10, ymd1[i])  # Granularity needed
     ana.parse_update()
