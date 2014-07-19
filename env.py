68c5ccbde7e25832ad29c408490814c601c4db52
cenmong <cenmong@gmail.com>
2014-07-19 10:16:58 +0800

    env modify

--- env.py
+++ env.py
@@ -31,13 +31,13 @@ daterange = [('20061225', 4, 168, '2006 taiwan cable cut', 0, 11),\
             ('20130317', 4, -1, '2013 Spamhaus DDoS', 9, 10),\
             ('20140601', 7, 188, 'for CDF in intro 2014', 10, 01),\
             ('20060601', 7, 154, 'for CDF in intro 2006', 11, 00),\
-            ('20130207', 4, -1, '2013 Northeastern U.S. Blackout', 12, 00),\
+            ('20130207', 4, -1, '2013 Northeastern U.S. Blackout', 12, 01),\
             ('20100413', 4, -1, '2010 Sea-Me undersea cable cut', 13, 00),\
             ('20120221', 4, -1, 'Australia route leakage', 14, 00),\
             ('20120807', 4, -1, 'Canada route leakage', 15, 00),\
-            ('20030124', 4, -1, '2003 Slammer worm', 16, 00),\
-            ('20130321', 4, -1, '20130322 EASSy/SEACOM Outages', 17, 00),\
-            ('20130213', 4, -1, '20130214 SEACOM Outages', 18, 00),\
+            ('20030124', 4, -1, '2003 Slammer worm', 16, 10),\
+            ('20130321', 4, -1, '20130322 EASSy/SEACOM Outages', 17, 10),\
+            ('20130213', 4, -1, '20130214 SEACOM Outages', 18, 10),\
             ('20110327', 4, -1, '20110328 Caucasus cable cut', 19, 00),\
             ('20121222', 4, -1, '20121223 Georgia-Russia cable cut', 20, 00),\
             ('20120224', 4, -1, '20120225 0913 TEAMS cable cut in east Africa', 21, 00),\
