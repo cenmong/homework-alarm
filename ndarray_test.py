42c5375b7d78301dc8d947415c37548e29c062c5
cenmong <cenmong@gmail.com>
2015-02-20 18:09:50 +0800

    matrix seems close to OK

--- ndarray_test.py
+++ ndarray_test.py
@@ -27,42 +27,73 @@ density = float(sum)/float(mymatrix.size)
 print 'density = ', density
 print '----------------'
 
+
 #----------------------------------------------
 # the thresholds
+thre_den = 0.8
+now_den = density
 
-#-----------------------------------------------
-# obtain candidate prefixes and monitors to delete
-pfx_1 = dict()
-mon_1 = dict()
-height = mymatrix.shape[0]
-width = mymatrix.shape[1]
-for i in xrange(0, height):
-    pfx_1[i] = mymatrix[i].sum()
-for i in xrange(0, width):
-    mon_1[i] = mymatrix[:,i].sum()
+while(now_den<thre_den):
+    #-------------------------
+    sum = float(np.sum(mymatrix))
+    size = float(mymatrix.size)
+    density = sum/mymatrix.size
 
-pfx_1 = sorted(pfx_1.items(), key=operator.itemgetter(1))
-print pfx_1
-min_pfx_1 = pfx_1[0][1]
-candidate_row_del = list()
-for item in pfx_1:
-    if item[1] == min_pfx_1:
-        candidate_row_del.append(item[0])
+    #-----------------------------------------------
+    # obtain candidate prefixes and monitors to delete
+    pfx_1 = dict()
+    mon_1 = dict()
+    height = mymatrix.shape[0]
+    width = mymatrix.shape[1]
+    for i in xrange(0, height):
+        pfx_1[i] = mymatrix[i].sum()
+    for i in xrange(0, width):
+        mon_1[i] = mymatrix[:,i].sum()
+
+    pfx_1 = sorted(pfx_1.items(), key=operator.itemgetter(1))
+    print pfx_1
+    min_pfx_1 = pfx_1[0][1]
+    candidate_row_del = list()
+    for item in pfx_1:
+        if item[1] == min_pfx_1:
+            candidate_row_del.append(item[0])
+        else:
+            break
+
+    mon_1 = sorted(mon_1.items(), key=operator.itemgetter(1))
+    print mon_1
+    min_mon_1 = mon_1[0][1]
+    candidate_col_del = list()
+    for item in mon_1:
+        if item[1] == min_mon_1:
+            candidate_col_del.append(item[0])
+        else:
+            break
+
+    print 'del pfx candi:', candidate_row_del
+    print 'del mon candi:', candidate_col_del
+    #-------------------------------------------
+    # decide which to delete
+    pfx_dsize = float(len(candidate_row_del) * width)
+    pfx_dsum = 0.0
+    for index in candidate_row_del:
+        pfx_dsum += mymatrix[index].sum()
+    pfx_compare = ((sum-pfx_dsum)/(size-pfx_dsize)-density)/pfx_dsize
+
+    mon_dsize = float(len(candidate_col_del) * height)
+    mon_dsum = 0.0
+    for index in candidate_col_del:
+        mon_dsum += mymatrix[:,index].sum()
+    mon_compare = ((sum-mon_dsum)/(size-mon_dsize)-density)/mon_dsize
+
+    print pfx_compare,mon_compare
+    if pfx_compare >= mon_compare:
+        print 'delete row:', candidate_row_del
+        mymatrix = np.delete(mymatrix,candidate_row_del,0)
+        now_den = (sum-pfx_dsum)/(size-pfx_dsize)
     else:
-        break
+        print 'delete col:', candidate_col_del
+        mymatrix = np.delete(mymatrix,candidate_col_del,1)
+        now_den = (sum-mon_dsum)/(size-mon_dsize)
 
-mon_1 = sorted(mon_1.items(), key=operator.itemgetter(1))
-print mon_1
-min_mon_1 = mon_1[0][1]
-candidate_col_del = list()
-for item in mon_1:
-    if item[1] == min_mon_1:
-        candidate_col_del.append(item[0])
-    else:
-        break
-
-print 'del pfx candi:', candidate_row_del
-print 'del mon candi:', candidate_col_del
-#-------------------------------------------
-# decide which to delete
-
+print mymatrix
