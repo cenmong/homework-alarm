a66662cbe349a2165da8adf5ed758b8660f363d9
cenmong <cenmong@gmail.com>
2015-05-22 16:24:28 +0800

    get the tiers of all ASes

--- mon_analysis.py
+++ mon_analysis.py
@@ -6,6 +6,8 @@ from plotter_class import Plotter
 
 import cmlib
 import os
+import subprocess
+from cStringIO import StringIO
 
 index_list = [20]
 
@@ -51,3 +53,62 @@ for i in index_list:
     print count
     print nation_count
     print tier_count
+
+    #--------------------
+    # get all ASes from RIB
+    f = open(my_period.rib_info_file, 'r')
+    ribfile = None
+    for line in f:
+        line = line.rstrip('\n')
+        co = line.split(':')[0]
+        if co == '':
+            ribfile = line.split(':')[1]
+    f.close()
+
+    as_set = set()
+    p = subprocess.Popen(['zcat', ribfile], stdout=subprocess.PIPE)
+    f = StringIO(p.communicate()[0])
+    assert p.returncode == 0
+    for line in f:
+        line = line.rstrip('\n')
+        try:
+            aspath = line.split('|')[6]
+        except:
+            continue
+        aslist = aspath.split()
+        for x in aslist:
+            try:
+                asn = int(x)
+            except:
+                continue
+            as_set.add(asn)
+    f.close()
+
+    print 'AS quantity: ', len(as_set)
+
+    my_period.get_as2cc_file()
+    as2cc = my_period.get_as2cc_dict()
+    as2tier = dict()
+    for a in as2cc:
+        if a in tier1_asn:
+            as2tier[a] = 1
+            continue
+        cc = as2cc[a]
+        if cc < 0:
+            as2tier[a] = -1 #unknown
+        elif cc <= 4:
+            as2tier[a] = 999
+        elif cc <= 50:
+            as2tier[a] = 3
+        else:
+            as2tier[a] = 2
+
+    count_dict = dict()
+    for a in as2tier:
+        tier = as2tier[a]
+        try:
+            count_dict[tier] += 1
+        except:
+            count_dict[tier] = 1
+
+    print count_dict
